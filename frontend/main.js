254aa83e5844f5ec1a158650ac217ac0b8782899
sushant <github@cyrocoders.ml>
2021-08-01 09:38:10 +0530

    Auto Redirect To /signup For Unauthenticated Users

--- frontend/main.js
+++ frontend/main.js
@@ -11,3 +11,27 @@ measurementId: "G-WHVJ27NH21"
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 firebase.auth();
+authEndpoints = [
+    "/frontend/login/",
+    "/frontend/signup/"
+];
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        console.log(user);
+    } else {
+        if(getPres(document.location.pathname, authEndpoints) === false) {
+            document.location.replace("/frontend/signup/")
+        }
+    }
+});
+
+function getPres(obj,lst) {
+    for(i = 0; i < lst.length; i++) {
+        data = lst[i]
+        if(obj === data) {
+            console.log({obj,data})
+            return true
+        }
+    }
+    return false;
+}
