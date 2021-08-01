13dd4306775f805b438f9eea527a79749ffaf48e
sushant <github@cyrocoders.ml>
2021-08-01 15:32:34 +0530

    something

--- frontend/index.js
+++ frontend/index.js
@@ -7,16 +7,20 @@ firebase.database().ref('shop').on('value',(snapshot) => {
 
 function genCard(item) {
     console.log(item.val())
+    nItem = {}
+    item.forEach(function(data) {
+        nItem[data.key] = data.val()
+    });
     card = document.createElement("div");
     card.setAttribute("id", item.key);
     card.setAttribute("onclick", "sell(this);");
     image = document.createElement("img");
-    image.src = item.child("image").val();
+    image.src = nItem["price"];
     image.setAttribute("width", "200px");
     image.setAttribute("height", "200px");
     card.append(image);
     textB = document.createElement("div");
-    textB.append(document.createElement("h1").append(item.val()["title"]));
+    textB.append(document.createElement("h1").append(nItem["price"]));
     card.append(textB);
     return card;
 }
